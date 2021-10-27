d2036c9e6545fbdd3d1b45d8a7cdd8ada7687c28
Harshit Varma <varmaharshit2@gmail.com>
2021-10-28 00:48:23 +0530

    added more results

--- ChampSim/run.sh
+++ ChampSim/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 BIN="bimodal-no-ip_stride-ip_stride-no-lru-1core"
+RES=$1
 
 ./build_champsim.sh bimodal no ip_stride ip_stride no lru 1
 
 ./run_champsim.sh $BIN 10 10 trace.champsimtrace.xz
 
-mv results_10M/*.txt ../results/prefetch_throttling_result/
+mv results_10M/*.txt ../results/prefetch_throttling_result/$1.txt
